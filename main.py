4c89bff2e1ceb3f9de4d20fc829823a7e0e4eb3d
LeivestadDev <aune.leivestad@gmail.com>
2026-01-15 00:23:09 +0100

    Update main.py
    Add sms test number

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+from fastapi import FastAPI, Request
+from sms import send_sms
+
+app = FastAPI()
+
 @app.get("/")
 def health():
     return {"status": "ok"}
-from sms import send_sms
 
 @app.get("/test-sms")
 def test_sms():
     send_sms("+4795330248", "Test fra Front SMS Gateway")
     return {"status": "sent"}
-
