24f9c90f1fc74cf3efa6012567f5b2fcc57a5e42
LeivestadDev <aune.leivestad@gmail.com>
2026-01-16 00:17:13 +0100

    Update sms.py

--- sms.py
+++ sms.py
@@ -8,25 +8,19 @@ SERVICE_ID = os.getenv("FRONT_SERVICE_ID")
 PASSWORD = os.getenv("FRONT_GATEWAY_PASSWORD")
 SENDER = os.getenv("SENDER_NUMBER")
 
-
 def send_sms(to: str, message: str):
-    if not SERVICE_ID or not PASSWORD or not SENDER:
-        raise ValueError("Manglende Front credentials")
-
     payload = {
         "serviceid": SERVICE_ID,
         "fromid": SENDER,
         "phoneno": to,
-        "txt": message,
+        "txt": message
     }
 
     response = requests.post(
         FRONT_API_URL,
         data=payload,  # VIKTIG: data, ikke json
         auth=HTTPBasicAuth(SERVICE_ID, PASSWORD),
-        timeout=10,
+        timeout=10
     )
 
     print("FRONT RESPONSE:", response.status_code, response.text)
-
-    return response
