e66492dfae6223363e65de5bdb19299b8cfeb11f
LeivestadDev <aune.leivestad@gmail.com>
2026-01-15 01:03:50 +0100

    Update main.py
    Add incoming SMS endpoint

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ app = FastAPI()
 @app.get("/")
 def health():
     return {"status": "ok"}
+
+@app.get("/incoming-sms")
+def incoming_sms(request: Request):
+    params = dict(request.query_params)
+
+    print("INNKOMMENDE SMS MOTTATT")
+    print("DATA:", params)
+
+    return {"status": "received"}
+
