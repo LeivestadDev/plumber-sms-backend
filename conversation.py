096048c8f7e0579f3ec77729f94b377fd61b45f3
LeivestadDev <aune.leivestad@gmail.com>
2026-01-16 01:46:06 +0100

    Update conversation.py

--- conversation.py
+++ conversation.py
@@ -1,7 +1,7 @@
 conversations = {}
 
 def get_state(phone):
-    return conversations.get(phone, {"step": "problem", "data": {}})
+    return conversations.get(phone, {"step": "start", "data": {}})
 
 def update_state(phone, step, data):
     conversations[phone] = {
